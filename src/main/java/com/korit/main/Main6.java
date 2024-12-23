10d681993ef13c23c1874176ca79c0d437ff3a2d
HONGMUNIL <bear4564@naver.com>
2024-12-23 09:39:08 +0900

    Main6

---
+++ src/main/java/com/korit/main/Main6.java
@@ -0,0 +1,27 @@
+package com.korit.main;
+
+
+import java.util.function.*;
+
+public class Main6 {
+
+    public static void main(String[] args) {
+        //Runnable (Main 메소드 역할)
+        Runnable runnable;
+
+        //자주쓰는 것들
+        Consumer<String> consumer;
+        Supplier<String> supplier;
+        Function<String, String> function;
+        Predicate<String> predicate;
+        BiConsumer<Integer, String> biConsumer;
+        BiFunction<Integer, Integer, String> biFunction;
+        BiPredicate<Integer, Integer> biPredicate;
+        UnaryOperator<Integer> unaryOperator;
+        BinaryOperator<Double> binaryOperator;
+
+
+
+
+    }
+}
